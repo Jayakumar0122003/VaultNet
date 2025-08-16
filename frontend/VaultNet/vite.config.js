09dfcac43e5b60bd4b42401cefddc79f76c413a0
Gubbala Jayakumar <115056837+jayak0776@users.noreply.github.com>
2025-08-16 14:00:52 +0530

    not found2

--- frontend/VaultNet/vite.config.js
+++ frontend/VaultNet/vite.config.js
@@ -4,12 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-  react({
-    include: "**/*.jsx",
-  }),
-  tailwindcss(),
-],
+  base: "/",
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
